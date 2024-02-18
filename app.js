8ddcbde1f96bfd19e4a2f4250a01b6242b861388
neerajnightcoder <kumar.neeraj.developer@gmail.com>
2024-02-18 16:46:15 +0530

    pages

--- app.js
+++ app.js
@@ -6,5 +6,9 @@ app.use("/", (req, res, next) => {
   res.send(<h1>Hi from Node server</h1>);
 });
 
+app.use("/products", (req, res) => {
+  res.send(<h1>here are some products</h1>);
+});
+
 const server = http.createServer(app);
 server.listen(5000);
